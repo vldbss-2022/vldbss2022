0133c8a039cdecb75b95709b437e422c0c200bf6
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2022-07-06 18:06:27 +0800

    fixup

---
+++ lab2/test_result.py
@@ -0,0 +1,21 @@
+import pytest
+import json
+
+class Test_simple():
+    def test_cost(self):
+        json_file = 'lab1/eval/results.json'
+        with open(json_file, 'r') as f:
+            data = json.load(f)
+        act = data.get('avi')
+        tidb = data.get('est')
+        learning = data.get('learning')
+        calibration = data.get('calibration')
+        if len(act) != len(tidb):
+            assert False
+        if [i for i in learning if i <= 0]:
+            assert False
+
+
+
+if __name__ == '__main__':
+    pytest.main(['test_result.py']) 
